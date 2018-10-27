4727425003dc04b675c41e665b06d2a61ed665ed
benheid <benheid@users.noreply.guthub.com>
2018-10-27 21:30:33 +0200

    working matrix_profile calculation, solved some numerical issues (hacky solution)

--- src/util.py
+++ src/util.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 def slidingDotProduct(Q, T):
+    #T = T.get_values()
+    T.resize(T.shape[0])
+    #Q = Q.get_values()
+    Q.resize(Q.shape[0])
     n = len(T)
     m = len(Q)
     Q_reversed = Q[::-1]
@@ -14,7 +18,7 @@ def timeseries_mean_stddev(T : np.ndarray, lag):
     :return: Array of mean and array of stddev
     """
     n = len(T)
-    t_padded = np.concatenate(([0], T, np.zeros(lag)))
+    t_padded = np.concatenate((np.zeros((1, 1)), T, np.zeros((lag, 1))))
     t_cum = np.cumsum(t_padded)
     t2_cum = np.cumsum(t_padded * t_padded)
     t_sum = t_cum[lag:n + 1] - t_cum[:n + 1 - lag] 
@@ -23,9 +27,26 @@ def timeseries_mean_stddev(T : np.ndarray, lag):
     t_sig = np.sqrt((t2_sum / lag) - (t_mu * t_mu))
     return t_mu, t_sig
 
-def mass(Q, T):
-    QT = slidingDotProduct(Q, T)
-    #mean_q, stddev_q, mean_T, sigma_T = computeMeanStd()
+def calculateDistanceProfile(Q, T, QT, T_mean, T_dev, mean_q, stddev_q):
+    m = len(Q)
+    var1 = QT[m-1:len(T)] - m * mean_q * T_mean
+    std = m * T_dev * stddev_q
+    return np.sqrt(np.abs(2* m * (1 - var1 / std)))
 
-def computeMeanStd():
-    pass
+def mass(Q, T, T_mean, T_dev):
+    QT = slidingDotProduct(Q, T)
+    mean_q = np.mean(Q)
+    stddev_q = np.std(Q)
+    return calculateDistanceProfile(Q, T, QT, T_mean, T_dev, mean_q, stddev_q)
+
+def stamp(Ta, Tb, m):
+    nb = len(Tb) - m + 1
+    pab = np.array([np.inf for i in range(nb)])
+    iab = np.zeros(nb)
+    t_mean, t_sig = timeseries_mean_stddev(Ta, m)
+    for idx in range(nb):
+        D = mass(Tb[idx: idx + m], Ta, t_mean, t_sig)
+        iab[pab > D] = idx
+        pab[pab > D] = D[pab > D]
+    return pab, iab
+
