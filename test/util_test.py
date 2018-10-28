a07707f3badf3ae7fe79663ac0f514fea0f93b47
benHeid <benHeid@users.noreply.github.com>
2018-10-28 16:28:23 +0100

    Feature/ci pipeline (#3)
    * add .travis.yml
    
    * update travis
    
    * update travis
    
    * update travis
    
    * working matrix_profile calculation, solved some numerical issues (hacky solution)
    
    * Implement Matrix Profile (#2)
    
    * Feature/ci pipeline (#1)
    
    * add .travis.yml
    
    * Update README.md

--- test/util_test.py
+++ test/util_test.py
@@ -1,6 +1,7 @@
 from pytest import fixture
-from src.util import slidingDotProduct, timeseries_mean_stddev
+from src.util import slidingDotProduct, timeseries_mean_stddev, calculateDistanceProfile
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 @fixture
 def getArray():
@@ -15,9 +16,21 @@ class TestUtil(object):
         assert np.all(np.round(ab).astype(np.integer) == np.array([6, 17, 32, 29, 29, 32, 23, 12]))
 
     def test_timeseries_mean_stddev(self):
-        a = np.array([1,2,3,1,2,3,1,2,3])
+        a = np.array([[1],[2],[3],[1],[2],[3],[1],[2],[3]])
         mean, stddev = timeseries_mean_stddev(a, 3)
         assert len(mean) == 7
         assert len(stddev) == 7
         assert np.all(np.round(mean) == np.array([2.0,2.0,2.0,2.0,2.0,2.0,2.0]))
         assert np.all(np.round(stddev) == np.array([1.0,1.0,1.0,1.0,1.0,1.0,1.0])) 
+
+    #def test_calculateDistanceProfile(self):
+    #    scaler = StandardScaler()
+    #    a = np.array([[1.0],[2.0],[1000.0]])
+    #   # a = scaler.fit_transform(a)
+    #    b = a[0:2]
+#
+    #    m, s = timeseries_mean_stddev(a, 2)
+    #    dist = calculateDistanceProfile(b, a, slidingDotProduct(b,a), m, s, np.mean(b), np.std(b))
+    #    assert len(dist) == 2
+    #    assert np.all(dist == [0,np.sqrt(2)])
+
